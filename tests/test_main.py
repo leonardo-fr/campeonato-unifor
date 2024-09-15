f3c9d088602ee187a0518dfc5438916c957cd10a
Adilson Rodrigues <100288155+Adilsonjvr@users.noreply.github.com>
2024-09-15 13:04:50 -0300

    Ajustes de teste da main

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,18 +6,20 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-# Importa a função main
+#Importa a função main
 from main import main
 
-class TestMainFunction(unittest.TestCase):
-    # Entrada de 85 simulada.
+#Cria uma classe de teste para verificar se a main finalizou corretamente
+class MainTest(unittest.TestCase):
+    #Entrada de 85 para simular uma pontuacao mínima
     @patch('builtins.input', side_effect=[85, 's', 1])
     #Serve para capturar o que seria exibido no console
-    @patch('sys.stdout', new_callable=StringIO)  #Redireciona a saída padrão
+    @patch('sys.stdout', new_callable=StringIO)
+
     def test_main_function_output(self, mock_stdout, _):
         main()
         
-        # Captura a saída gerada
+        #Captura a saída simulada gerada
         output = mock_stdout.getvalue()
         
         #Serve para indicar que o programa chegou ao final de sua execução
